d161bec9e9fd8be5029eb0ec351fff6a83852b98
sartajqamar <sartajqamar111@gmail.com>
2025-12-18 10:25:54 +0500

    upgraded files

--- app/src/main/java/com/example/ghazalapp/DetailDataGhazalActivity.kt
+++ app/src/main/java/com/example/ghazalapp/DetailDataGhazalActivity.kt
@@ -261,6 +261,7 @@ class DetailDataGhazalActivity : AppCompatActivity() {
             Log.e("PDFCreator", "ioException:$")
         }
     }
+        @Deprecated("This method has been deprecated in favor of using the\n      {@link OnBackPressedDispatcher} via {@link #getOnBackPressedDispatcher()}.\n      The OnBackPressedDispatcher controls how back button events are dispatched\n      to one or more {@link OnBackPressedCallback} objects.")
         override fun onBackPressed() {
             super.onBackPressed()
         }
